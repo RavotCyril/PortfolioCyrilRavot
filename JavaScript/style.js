8e541c76924f21e43f81ac1ae10a53aee0bcd859
GAMER <cyril.ravot@hotmail.fr>
2021-09-01 02:16:08 +0200

    Page Projet  Ok. Implantation CSS et Java-Script  : ok.   Html boostrap Ok. Fonctionnement ok. Reste projet 5-6-7-8  à mettre à jour une fois terminé.

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -21,6 +21,8 @@ let divLienProjetGroupomania = document.getElementById("divLienProjetGroupomania
 let divLienProjetPeinture = document.getElementById("divLienProjetPeinture");
 
 
+/* Function de tous les bouttons pour utliser le display none - Block quand on clique sur les images dans les boutons. */
+
 bouttonLogoReservia.addEventListener("click", () => {
     if (getComputedStyle(divLienProjetReservia).display != "none") {
         divLienProjetReservia.style.display = "none";
@@ -29,3 +31,60 @@ bouttonLogoReservia.addEventListener("click", () => {
         bouttonLogoReservia.style.display = "none";
     }
 })
+bouttonLogoOhmyfood.addEventListener("click", () => {
+    if (getComputedStyle(divLienProjetOhmyfood).display != "none") {
+        divLienProjetOhmyfood.style.display = "none";
+    } else {
+        divLienProjetOhmyfood.style.display = "block";
+        bouttonLogoOhmyfood.style.display = "none";
+    }
+})
+bouttonLogoPageOrigineLaChouetteAgence.addEventListener("click", () => {
+    if (getComputedStyle(divLienProjetPageOrigineLaChouetteAgence).display != "none") {
+        divLienProjetPageOrigineLaChouetteAgence.style.display = "none";
+    } else {
+        divLienProjetPageOrigineLaChouetteAgence.style.display = "block";
+        bouttonLogoPageOrigineLaChouetteAgence.style.display = "none";
+    }
+})
+bouttonLogoPageCorrectifLaChouetteAgence.addEventListener("click", () => {
+    if (getComputedStyle(divLienProjetPageCorrectifLaChouetteAgence).display != "none") {
+        divLienProjetPageCorrectifLaChouetteAgence.style.display = "none";
+    } else {
+        divLienProjetPageCorrectifLaChouetteAgence.style.display = "block";
+        bouttonLogoPageCorrectifLaChouetteAgence.style.display = "none";
+    }
+})
+bouttonLogoOrinoco.addEventListener("click", () => {
+    if (getComputedStyle(divLienProjetOrinoco).display != "none") {
+        divLienProjetOrinoco.style.display = "none";
+    } else {
+        divLienProjetOrinoco.style.display = "block";
+        bouttonLogoOrinoco.style.display = "none";
+    }
+})
+bouttonLogoGroupomania.addEventListener("click", () => {
+    if (getComputedStyle(divLienProjetSoPeckocko).display != "none") {
+        divLienProjetSoPeckocko.style.display = "none";
+    } else {
+        divLienProjetSoPeckocko.style.display = "block";
+        bouttonLogoGroupomania.style.display = "none";
+    }
+})
+bouttonLogoSoPekocko.addEventListener("click", () => {
+    if (getComputedStyle(divLienProjetGroupomania).display != "none") {
+        divLienProjetGroupomania.style.display = "none";
+    } else {
+        divLienProjetGroupomania.style.display = "block";
+        bouttonLogoSoPekocko.style.display = "none";
+    }
+})
+bouttonLogoPeinture.addEventListener("click", () => {
+        if (getComputedStyle(divLienProjetPeinture).display != "none") {
+            divLienProjetPeinture.style.display = "none";
+        } else {
+            divLienProjetPeinture.style.display = "block";
+            bouttonLogoPeinture.style.display = "none";
+        }
+    })
+    /* ----------------------------------------------Fin Function de tous les bouttons  Fin--------------------------------------------*/
