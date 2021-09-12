4954f8a4be617e1244de9cb2a131d209f5f64655
GAMER <cyril.ravot@hotmail.fr>
2021-09-12 02:16:36 +0200

    Page responsive : Ok toute les pages. Footer réalisé : ok avec icons linkdin, téléphone , Email. Page projet : ok  et java script : ok. Reste à faire java script pour le formulaire et rajouter du contenu. Formation-Compétence.

---
+++ JavaScript/Nav.js
@@ -0,0 +1,12 @@
+            /* -------------------Function Navigation bouton mobile  Début----------------------------------*/
+
+            function NavigationMobile() {
+                let divNav = document.getElementById("myTopnav");
+                if (divNav.className === "Navigation-Mobile") {
+                    divNav.className += " responsive";
+                } else {
+                    divNav.className = "Navigation-Mobile";
+                }
+            }
+            NavigationMobile()
+                /* ---------------------- Function Navigation bouton mobile Fin-------------------------------------*/
---
+++ JavaScript/Projets.js
@@ -0,0 +1,96 @@
+  /* page Projet --> Function pour faire apparaitre et disparaitre les boutons des logos  */
+
+  function BouttonDynamiqueLogo() {
+
+      /* Variable de récupérations des ID dans le html des Bouttons Logo des projets pour pouvoir mettre le style None ( avec class ça ne fonctionne pas )  */
+
+      let bouttonLogoReservia = document.getElementById("bouttonLogoReservia");
+      let bouttonLogoOhmyfood = document.getElementById("bouttonLogoOhmyfood");
+      let bouttonLogoPageOrigineLaChouetteAgence = document.getElementById("bouttonLogoPageOrigineLaChouetteAgence");
+      let bouttonLogoPageCorrectifLaChouetteAgence = document.getElementById("bouttonLogoPageCorrectifLaChouetteAgence");
+      let bouttonLogoOrinoco = document.getElementById("bouttonLogoOrinoco");
+      let bouttonLogoGroupomania = document.getElementById("bouttonLogoGroupomania");
+      let bouttonLogoSoPekocko = document.getElementById("bouttonLogoSoPekocko");
+      let bouttonLogoPeinture = document.getElementById("bouttonLogoPeinture");
+
+      /* Variable de récupérations des ID dans le html des Liens des projets pour pouvoir mettre le style None  ( avec class ça ne fonctionne pas ) */
+
+      let divLienProjetReservia = document.getElementById("divLienProjetReservia");
+      let divLienProjetOhmyfood = document.getElementById("divLienProjetOhmyfood");
+      let divLienProjetPageOrigineLaChouetteAgence = document.getElementById("divLienProjetPageOrigineLaChouetteAgence");
+      let divLienProjetPageCorrectifLaChouetteAgence = document.getElementById("divLienProjetPageCorrectifLaChouetteAgence");
+      let divLienProjetOrinoco = document.getElementById("divLienProjetOrinoco");
+      let divLienProjetSoPeckocko = document.getElementById("divLienProjetSoPeckocko");
+      let divLienProjetGroupomania = document.getElementById("divLienProjetGroupomania");
+      let divLienProjetPeinture = document.getElementById("divLienProjetPeinture");
+
+
+      /* Function de tous les bouttons pour utiliser le display none - Block quand on clique sur les images dans les boutons. */
+
+      bouttonLogoReservia.addEventListener("click", () => {
+          if (getComputedStyle(divLienProjetReservia).display != "none") {
+              divLienProjetReservia.style.display = "none";
+          } else {
+              divLienProjetReservia.style.display = "block";
+              bouttonLogoReservia.style.display = "none";
+          }
+      })
+      bouttonLogoOhmyfood.addEventListener("click", () => {
+          if (getComputedStyle(divLienProjetOhmyfood).display != "none") {
+              divLienProjetOhmyfood.style.display = "none";
+          } else {
+              divLienProjetOhmyfood.style.display = "block";
+              bouttonLogoOhmyfood.style.display = "none";
+          }
+      })
+      bouttonLogoPageOrigineLaChouetteAgence.addEventListener("click", () => {
+          if (getComputedStyle(divLienProjetPageOrigineLaChouetteAgence).display != "none") {
+              divLienProjetPageOrigineLaChouetteAgence.style.display = "none";
+          } else {
+              divLienProjetPageOrigineLaChouetteAgence.style.display = "block";
+              bouttonLogoPageOrigineLaChouetteAgence.style.display = "none";
+          }
+      })
+      bouttonLogoPageCorrectifLaChouetteAgence.addEventListener("click", () => {
+          if (getComputedStyle(divLienProjetPageCorrectifLaChouetteAgence).display != "none") {
+              divLienProjetPageCorrectifLaChouetteAgence.style.display = "none";
+          } else {
+              divLienProjetPageCorrectifLaChouetteAgence.style.display = "block";
+              bouttonLogoPageCorrectifLaChouetteAgence.style.display = "none";
+          }
+      })
+      bouttonLogoOrinoco.addEventListener("click", () => {
+          if (getComputedStyle(divLienProjetOrinoco).display != "none") {
+              divLienProjetOrinoco.style.display = "none";
+          } else {
+              divLienProjetOrinoco.style.display = "block";
+              bouttonLogoOrinoco.style.display = "none";
+          }
+      })
+      bouttonLogoGroupomania.addEventListener("click", () => {
+          if (getComputedStyle(divLienProjetSoPeckocko).display != "none") {
+              divLienProjetSoPeckocko.style.display = "none";
+          } else {
+              divLienProjetSoPeckocko.style.display = "block";
+              bouttonLogoGroupomania.style.display = "none";
+          }
+      })
+      bouttonLogoSoPekocko.addEventListener("click", () => {
+          if (getComputedStyle(divLienProjetGroupomania).display != "none") {
+              divLienProjetGroupomania.style.display = "none";
+          } else {
+              divLienProjetGroupomania.style.display = "block";
+              bouttonLogoSoPekocko.style.display = "none";
+          }
+      })
+      bouttonLogoPeinture.addEventListener("click", () => {
+          if (getComputedStyle(divLienProjetPeinture).display != "none") {
+              divLienProjetPeinture.style.display = "none";
+          } else {
+              divLienProjetPeinture.style.display = "block";
+              bouttonLogoPeinture.style.display = "none";
+          }
+      })
+  }
+  BouttonDynamiqueLogo()
+      /* ----------------------------------------------Fin Function de tous les bouttons  Fin--------------------------------------------*/
