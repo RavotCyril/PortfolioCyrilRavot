4efa2406f28c47fd1d3102ad337b2b7ebeb9c303
Cyril ravot <cyril.ravot@hotmail.fr>
2022-06-28 23:46:26 +0200

    Défaut Contracte Page Projet: Réglé. Alert supprimés sur pas mal de page. Test Wave Google Chroome. Test plugin alert à supprimer en cours.

--- JavaScript/Nav.js
+++ JavaScript/Nav.js
@@ -1,12 +1,13 @@
-            /* -------------------Function Navigation bouton mobile  Début----------------------------------*/
+/* -------------------Function Navigation bouton mobile  Début----------------------------------*/
 
 function NavigationMobile() {
-     let divNav = document.getElementById("myTopnav");
-        if (divNav.className === "Navigation-Mobile") {
-                    divNav.className += " responsive";
-        } else {
-                    divNav.className = "Navigation-Mobile";
-     }
-    }
- NavigationMobile()     
- /* ---------------------- Function Navigation bouton mobile Fin-------------------------------------*/
+   let divNav = document.getElementById("myTopnav");
+   if (divNav.className === "Navigation-Mobile") {
+      divNav.className += " responsive";
+   } else {
+      divNav.className = "Navigation-Mobile";
+   }
+}
+NavigationMobile()
+
+/* ---------------------- Function Navigation bouton mobile Fin-------------------------------------*/
