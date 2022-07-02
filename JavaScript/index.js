da87022737a83fa4f95fec109c92a1429c633cdd
Cyril ravot <cyril.ravot@hotmail.fr>
2022-07-02 02:37:20 +0200

    test

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -7,6 +7,7 @@ puis de l'arrondir à la valeur la plus proche Exemple  2.5 = 3   1.1 =1
 // // Crée une condition de média qui cible des fenêtres d'au moins 375  pixels et en dessous de largeur ( BreakPoint en JavaScript)
 
 /* Lazy Load pour précharger le background-image que quand il est visible */
+
 document.addEventListener("DOMContentLoaded", function () {
 
      var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-bg"));
